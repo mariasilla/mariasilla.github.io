bca974ecec561a5f73268553ba68dc0e7b205474
Maria Silla <maria.sylla@gmail.com>
2017-08-17 18:22:29 -0400

    update contact form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
 $(function() {
 
+   $(window).resize(function(){
+      var width = $(window).width();
+      if (width < 600) {
+        // alert("width is less than 400");
+          $("#aboutmedescr_hidden").show();
+          $("#aboutmedescr").hide();
+      } else {
+          $("#aboutmedescr_hidden").hide();
+          $("#aboutmedescr").show();
+        // $("#aboutmedescr").off();
+        //  $("#aboutmedescr").show();
+        // alert("width is more than 400")
+      }
+   })
+
+
+
+
+  //  var modal = $("#myModal");
+   //
+  //  var modTrig = $("#modalTrigger");
+   //
+  //  var span = $(".close")[0];
+   //
+  //  modTrig.click(
+  //    function() {
+  //      modal.css("display", "block");
+  //     //  modal.style.dispay = "block";
+  //    });
+   //
+  //  span.click(
+  //    function() {
+  //      modal.hide();
+  //     //  modal.css("display", "none");
+  //     //  modal.style.display = "none";
+  //    })
+   //
+  //  $(window).click(
+  //    function(event) {
+  //       if (event.target == modal) {
+  //          modal.hide();
+  //         //  modal.css("display", "none");
+  //         //  modal.style.dispaly = "none";
+  //       }
+  //    })
+
+
 });
